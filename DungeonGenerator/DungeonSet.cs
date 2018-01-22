fe2c795d5aed9e26a36fe4f5db71664b5cfcb2e6
Carl Wells <carl@fivetotwelve.com>
2018-01-22 16:59:14 +0000

    Refactored the file structure

---
+++ DungeonGenerator/DungeonSet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace EL.Dungeon {
+    [CreateAssetMenu(fileName = "NewDungeonSet", menuName = "DungeonGenerator/DungeonSet", order = 1)]
+    public class DungeonSet : ScriptableObject {
+
+        public string name = "";
+
+        public List<Room> spawns = new List<Room>();
+        public List<Room> bosses = new List<Room>();
+        public List<Door> doors = new List<Door>();
+        public List<Room> roomTemplates = new List<Room>();
+        
+    }
+}
